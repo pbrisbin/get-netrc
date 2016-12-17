7b290e15d5f7bf08f1c7b1230f109979657973f0
patrick brisbin <pbrisbin@gmail.com>
2016-12-16 19:40:11 -0500

    Centralize host field accesses

--- src/Lib.hs
+++ src/Lib.hs
@@ -18,21 +18,21 @@ getNetRc Options{..} = either
     $ parseNetRc oFilePath oFileContents
 
 findHosts :: [Filter] -> NetRc -> [NetRcHost]
-findHosts fs = filter (\h -> all (matchesField h) fs) . nrHosts
-  where
-    matchesField NetRcHost{..} (Filter Name n) = nrhName `bsEq` n
-    matchesField NetRcHost{..} (Filter Login l) = nrhLogin `bsEq` l
-    matchesField NetRcHost{..} (Filter Password p) = nrhPassword `bsEq` p
-    matchesField NetRcHost{..} (Filter Account a) = nrhAccount `bsEq` a
-
-    bsEq bs = (== bs) . C8.pack
+findHosts fs = filter (\h -> all (matchesFilter h) fs) . nrHosts
 
 filterOutput :: [Field] -> [NetRcHost] -> Output
-filterOutput fs = MachineValues . map getFields
-    where
-        getFields n = map (getField n) fs
+filterOutput fs = MachineValues . map valuesAtFields
+  where
+    valuesAtFields h = map (valueAtField h) fs
 
-        getField NetRcHost{..} Name = Value Name $ C8.unpack nrhName
-        getField NetRcHost{..} Login = Value Login $ C8.unpack nrhLogin
-        getField NetRcHost{..} Password = Value Password $ C8.unpack nrhPassword
-        getField NetRcHost{..} Account = Value Account $ C8.unpack nrhAccount
+matchesFilter :: NetRcHost -> Filter -> Bool
+matchesFilter h (Filter f v) = accessField f h == v
+
+valueAtField :: NetRcHost -> Field -> Value
+valueAtField h f = Value f $ accessField f h
+
+accessField :: Field -> (NetRcHost -> String)
+accessField Name = C8.unpack . nrhName
+accessField Login = C8.unpack . nrhLogin
+accessField Password = C8.unpack . nrhPassword
+accessField Account = C8.unpack . nrhAccount
